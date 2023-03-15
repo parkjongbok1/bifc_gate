097284bad3826c285be992e06986675f7a8e80da
복 <103090499+parkjongbok1@users.noreply.github.com>
2023-03-15 18:35:34 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-  $(".gate_slider").slick({
-    dots: false,
-    infinite: true,
-    arrows: true,
-    prevArrow: $(".gate_p"),
-    nextArrow: $(".gate_n"),
-    autoplay: true,
-    autoplaySpeed: 3200,
-    speed: 1300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-  });
+  // $(".gate_slider").slick({
+  //   dots: false,
+  //   infinite: true,
+  //   arrows: true,
+  //   prevArrow: $(".gate_p"),
+  //   nextArrow: $(".gate_n"),
+  //   autoplay: true,
+  //   autoplaySpeed: 3200,
+  //   speed: 1300,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   fade: true,
+  // });
 });
